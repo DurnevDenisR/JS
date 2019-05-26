f521bbf5ebaab9044dfcf7e1d22532326b7aa666
DurnevDenisR <44579103+DurnevDenisR@users.noreply.github.com>
2019-05-27 01:39:38 +0300

    Add files via upload

--- index2.js
+++ index2.js
@@ -1,17 +1,38 @@
+let answer;
+let question;
+let jokes=[];
+localStorage.clear();
+
 function loadJoke() {
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://official-joke-api.appspot.com/jokes/random', false);
-  xhr.send();
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://official-joke-api.appspot.com/jokes/random', false);
+    xhr.send();
 
-  if (xhr.status != 200) {
-    alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
-  } else {
+    if (xhr.status == 200) {
+        let jsonResponse = JSON.parse(xhr.responseText);
+        question = jsonResponse.setup;
+        answer = jsonResponse.punchline;
 
-    let serialObj = JSON.stringify(xhr.responseText, ['setup', 'punchline']);
-    localStorage.setItem('Favorites', 'serialObj');
-    let jsonResponse=JSON.parse(xhr.responseText);
-    alert(localStorage.getItem('Favorites'));
-    alert(jsonResponse.setup);
-    alert(jsonResponse.punchline);
-  }
+        let an = document.getElementById("a");
+        an.innerText = answer;
+        let qu = document.getElementById("q");
+        qu.innerText = question;
+    } else {
+        alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+    }
+}
+
+function addToFavorite() {
+    localStorage.setItem(question,answer);
+}
+
+function showFavorite() {
+    let favorite = document.getElementById("favorite");
+    favorite.innerHTML = null;
+    let keys=Object.keys(localStorage);
+    keys.forEach((key)=>{
+        let el1 = document.createElement('li');
+        el1.innerText = key+" "+localStorage.getItem(key);
+        favorite.appendChild(el1);
+    })
 }
